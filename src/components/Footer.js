5c6e84460f10a8449a8945defe72c1083ca8aa2b
emilesherrott <emilesherrott@gmail.com>
2021-08-13 23:29:21 +0100

    Home page & Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,84 @@
 import React from 'react'
 
+import JavaScriptLogo from './footerLanguages/JavaScriptLogo'
+import ReactLogo from './footerLanguages/ReactLogo'
+import HTMLLogo from './footerLanguages/HTMLLogo'
+import CSSLogo from './footerLanguages/CSSLogo'
+import SassLogo from './footerLanguages/SassLogo'
+import BootstrapLogo from './footerLanguages/BootstrapLogo'
+import BulmaLogo from './footerLanguages/BulmaLogo'
+
+
+import NodeLogo from './footerLanguages/NodeLogo'
+import ExpressLogo from './footerLanguages/ExpressLogo'
+import MongoLogo from './footerLanguages/MongoLogo'
+// import MongooseLogo from './footerLanguages/MongooseLogo'
+import PythonLogo from './footerLanguages/PythonLogo'
+import DjangoLogo from './footerLanguages/DjangoLogo'
+import PostgreSQLLogo from './footerLanguages/PostgreSQLLogo'
+
+
+import GitLogo from './footerLanguages/GitLogo'
+import GitHubLogo from './footerLanguages/GitHubLogo'
+import YarnLogo from './footerLanguages/YarnLogo'
+
 const Footer = () => {
 
   return (
     <div className="container scss-footer">
-      <h3>emilesherrott@gmail.com</h3>
-      <h3>github.com/emilesherrott</h3>
-      <h3>linkedin.com/in/emilesherrott/</h3>
+      <div className="row scss-footer-body">
+        <div className="col col-lg-6 scss-footer-contactinfo">
+          <a href="emilesherrott@gmail.com" target="_blank">
+            <h3 className="scss-footer-text">emilesherrott@gmail.com</h3>
+          </a>
+          <a href="https://github.com/emilesherrott" target="_blank" rel="noreferrer">
+            <h3 className="scss-footer-text">github.com/emilesherrott</h3>
+          </a>
+          <a href="https://www.linkedin.com/in/emilesherrott/" target="_blank" rel="noreferrer">
+            <h3 className="scss-footer-text">linkedin.com/in/emilesherrott/</h3>
+          </a>
+        </div>
+        <div className="col col-lg-6 scss-footer-logo-section">
+
+          <div className="scss-footer-skills">
+            <p>Fontend:</p>
+            <div className="d-flex">
+              <JavaScriptLogo />
+              <ReactLogo />
+              <HTMLLogo />
+              <CSSLogo />
+              <SassLogo />
+              <BootstrapLogo />
+              <BulmaLogo />
+            </div>
+          </div>
+
+          <div className="scss-footer-skills">
+            <p>Backend:</p>
+            <div className="d-flex">
+              <NodeLogo />
+              <ExpressLogo />
+              <MongoLogo />
+              {/* <MongooseLogo /> */}
+              <PythonLogo />
+              <DjangoLogo />
+              <PostgreSQLLogo />
+            </div>
+          </div>
+
+          <div className="scss-footer-skills">
+            <p>Management & Deployment:</p>
+            <div className="d-flex">
+              <GitLogo />
+              <GitHubLogo />
+              <YarnLogo />
+            </div>
+          </div>
+
+
+
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/footerLanguages/MongooseLogo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import mongooseLogo from '../../assets/footer/mongoose.png'
+
+const MongooseLogo = () => {
+
+  return (
+    <div className="scss-language-logo">
+      <img src={mongooseLogo} alt="mongoose logo" className="scss-language-mongoose-logo" />
+    </div>
+  )
+}
+
+export default MongooseLogo 
